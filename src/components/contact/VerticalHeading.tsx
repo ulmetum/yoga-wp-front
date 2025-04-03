1029efe21f386b3f461b4839006902e9df17a189
ulmetum <ulmetum@gmail.com>
2025-04-03 14:08:14 +0200

    style: Añadidas clases personalizadas custom-delay

--- src/components/contact/VerticalHeading.tsx
+++ src/components/contact/VerticalHeading.tsx
@@ -1,20 +1,20 @@
 export const VerticalHeading = () => {
   return (
-    <div>
+    <div className=''>
       <h2 className='absolute top-1/2 z-10 -mt-5 flex py-20 mb-0 w-1/2 -translate-y-1/2 flex-col justify-center bg-white text-right'>
-        <span className='mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
+        <span className='opacity-0 fade-in custom-delay-500 mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
           Puedes
         </span>
-        <span className='mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
+        <span className='opacity-0 fade-in custom-delay-600 mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
           ponerte
         </span>
-        <span className='mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
+        <span className='opacity-0 fade-in custom-delay-700 mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
           en
         </span>
-        <span className='mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
+        <span className='opacity-0 fade-in custom-delay-800 mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
           contacto
         </span>
-        <span className='mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
+        <span className='opacity-0 fade-in custom-delay-900 mr-4 inline-block text-right font-heading font-bold uppercase !leading-[1.125] [font-size:var(--fs-600)] sm:text-6xl'>
           conmigo
         </span>
       </h2>
