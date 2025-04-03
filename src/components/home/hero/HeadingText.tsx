ea237e54102c8c52c6d6c2500ab918fe9cc3a486
ulmetum <ulmetum@gmail.com>
2025-04-03 21:04:32 +0200

    refactor: Se elimina Motion One para sustituirlo por Tailwind

--- src/components/home/hero/HeadingText.tsx
+++ src/components/home/hero/HeadingText.tsx
@@ -1,33 +1,24 @@
-'use client'
-
 import { cn } from '@/utils/mergeClass'
-import { motion } from 'motion/react'
 
 export const HeadingText = ({
   classNames,
   children,
-  delayAnimation,
+  animationDelay,
 }: {
   classNames?: string
   children: React.ReactNode
-  delayAnimation?: number
+  animationDelay?: number
 }) => {
   return (
     <div className={cn('overflow-hidden leading-none', classNames)}>
-      <motion.h1
-        initial={{ y: '135%' }}
-        animate={{ y: 0 }}
-        transition={{
-          duration: 1,
-          delay: delayAnimation,
-          ease: [0.83, 0, 0.17, 1],
-        }}
+      <h1
+        style={{ animationDelay: `${animationDelay}s` }}
         className={
-          'relative m-1 font-headings tracking-wide font-bold uppercase leading-[1.15] [font-size:var(--fs-700)] sm:[font-size:var(--fs-700)]'
+          'translate-y-full animate-slideInUp relative m-1 font-headings tracking-wide font-bold uppercase leading-[1.15] [font-size:var(--fs-700)] sm:[font-size:var(--fs-700)]'
         }
       >
         {children}
-      </motion.h1>
+      </h1>
     </div>
   )
 }
